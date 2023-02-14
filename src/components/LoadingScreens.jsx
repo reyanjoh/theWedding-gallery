e053eb7218db7c04cdeaee770533e8e0467e342f
reyan <2001103609@student.buksu.edu.ph>
2023-02-14 10:13:42 +0800

    added GalleryCardSliderLoadingScreen compontent

---
+++ src/components/LoadingScreens.jsx
@@ -0,0 +1,56 @@
+import './LoadingScreens.css'
+
+
+
+export const LoadingImagesComponenet = () => {
+  return (
+    <>      
+      <div className='LoadingImagesComponenet-head width62'>
+        <div className='LoadingImagesComponenet-head-title flex flex-column flex-center'>
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-title'></div>
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-title last'></div>
+
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-author'></div>
+        </div>
+
+        <div className='LoadingImagesComponenet-head-description'>
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-description'></div>
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-description'></div>
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-description'></div>
+          <div className='LoadingImagesComponenet LoadingImagesComponenet-description last'></div>
+        </div>
+      </div>
+      {/* <div></div>
+      <div></div> */}
+      <div className='LoadingImagesComponenet-Img-section'>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
+      </div>
+    
+    </>
+  )
+}
+
+
+export const GalleryCardSliderLoadingScreen = () => {
+  return (
+    <div className='GalleryCardSliderLoadingScreen flex flex-column flex-center'>
+        <div className="LoadingImagesComponenet GalleryCardSliderLoadingScreen-studio-name">
+
+        </div>
+        <div className="LoadingImagesComponenet GalleryCardSliderLoadingScreen-img">
+
+        </div>
+    </div>
+  )
+}
+
+
--- src/pages/gallery/components/galleryPost/component/ImagesComponenet.jsx
+++ src/pages/gallery/components/galleryPost/component/ImagesComponenet.jsx
@@ -1,6 +1,4 @@
 import { v4 as uuidv4 } from 'uuid';
-
-
 import React from 'react'
 
 export const ImagesComponenet = ({data, loading}) => {
@@ -25,43 +23,3 @@ export const ImagesComponenet = ({data, loading}) => {
     </>
   )
 }
-
-
-export const LoadingImagesComponenet = () => {
-  return (
-    <>      
-      <div className='LoadingImagesComponenet-head width62'>
-        <div className='LoadingImagesComponenet-head-title flex flex-column flex-center'>
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-title'></div>
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-title last'></div>
-
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-author'></div>
-        </div>
-
-        <div className='LoadingImagesComponenet-head-description'>
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-description'></div>
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-description'></div>
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-description'></div>
-          <div className='LoadingImagesComponenet LoadingImagesComponenet-description last'></div>
-        </div>
-      </div>
-      {/* <div></div>
-      <div></div> */}
-      <div className='LoadingImagesComponenet-Img-section'>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-        <div className='LoadingImagesComponenet LoadingImagesComponenet-Img'></div>
-      </div>
-    
-    </>
-    
-  )
-}
-
