be25a69d8a36762b7c0a586222c48f73b7403bdc
reyan <2001103609@student.buksu.edu.ph>
2023-02-15 19:38:48 +0800

    added the submit button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import Nav from './Nav'
 
 const Header = () => {
   return (
     <div className='header flex flex-column padding-2-6'>
         <div className='logo header flex flex-column flex-align-center'><a href="/">The wedding<br/> gallery</a></div>
+
+        <NavLink className={'submit padding-0-1 font-small'} to={'/submit'}>Submit</NavLink>
+        {/* <NavLink className={'nav padding-0-1 font-small'} to={'/'}>Home</NavLink> */}
         <Nav />
     </div>
   )
