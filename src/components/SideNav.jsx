6372ea8e4cb266f10368cf3592197b8f220f77e6
reyan <2001103609@student.buksu.edu.ph>
2023-02-17 09:49:15 +0800

    not-available cursor @blogs&submit button

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const SideNav = () => {
   return (
     <div className='SideNav'>
-        <NavLink className={'uppercase submit padding-0-1 font-small'} to={'/submit'}>Submit</NavLink>
-        <NavLink className={'uppercase nav padding-0-1 font-small'} to={'/blogs'}>Blogs</NavLink>  
+        <NavLink className={'uppercase submit padding-0-1 font-small not-available'} to={'/submit'}>Submit</NavLink>
+        <NavLink className={'uppercase nav padding-0-1 font-small not-available'} to={'/blogs'}>Blogs</NavLink>  
         <NavLink className={'uppercase nav padding-0-1 font-small'} to={'/career'}>Career</NavLink>
     </div>
   )
